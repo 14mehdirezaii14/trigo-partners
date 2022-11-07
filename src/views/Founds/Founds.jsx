3619b5b8f03724296c43f882087261b0e80b5336
MehdiRezaii <99mehdirezaii99@gmail.com>
2022-11-07 18:57:52 +0330

    Added HeaderHalf To Founds Page

--- src/views/Founds/Founds.jsx
+++ src/views/Founds/Founds.jsx
@@ -1,7 +1,18 @@
+import HeaderHalf from "../../components/header/HeaderHalf";
+const heroContent = {
+    category: 'Founds', title: 'Founds', src: '/assets/img/project/project1/AboutHead.png'
+}
+
 const Founds = () => {
-    return(
+    return (
         <>
-        Founds
+            <HeaderHalf heroContent={heroContent}
+                parallax={{ yPercent: 30, scale: 1.1 }}
+                textButton=""
+                href="/"
+            >
+                {/* <strong className="color-heading">Seohee Lee</strong> <span> - September 26th 2017</span> */}
+            </HeaderHalf>
         </>
     )
 }
